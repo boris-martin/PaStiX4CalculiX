bcc615f7041297fa60d10b9e9c62d1948ab4b794
Boris Martin <84379125+boris-martin@users.noreply.github.com>
2021-08-19 13:09:17 +0200

    Update my_make_pastix.sh

--- my_make_pastix.sh
+++ my_make_pastix.sh
@@ -9,14 +9,15 @@ cmake   \
         -DBLAS_DIR=~/OpenBLAS_i8 \
 	-DHWLOC_DIR=~/PaStiX/hwloc_i8 \
 	-DHWLOC_LIBDIR=~/PaStiX/hwloc_i8/lib/ \
+	-DHWLOC_INCDIR=~/PaStiX/hwloc_i8/include/ \
 	-DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.2 \
 	-DCMAKE_INSTALL_PREFIX=~/PaStiX/pastix_i8 \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DPASTIX_WITH_PARSEC=ON \
 	-DPARSEC_DIR=~/PaStiX/parsec_i8 \
 	-DSCOTCH_DIR=~/PaStiX/scotch_i8 \
-	-DPASTIX_WITH_CUDA=OFF \
-	-DPASTIX_WITH_MPI=ON \
+	-DPASTIX_WITH_CUDA=ON \
+	-DPASTIX_WITH_MPI=OFF \
 	-DCUDA_DIR=/usr/local/cuda-10.2 \
 	-DPASTIX_ORDERING_SCOTCH=ON \
 	-DCMAKE_C_COMPILER=gcc \
